46b84937ca7c6064481ce62442d3add3af448fed
charan <charan@CHARAN-PC>
2018-02-25 19:04:40 -0800

    Added Sample soap client

---
+++ Sample2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SoapClientManager
+{
+    public class Sample2
+    {
+       
+        /// <summary>
+        /// Execute a Soap WebService call
+        /// </summary>
+        public static void Execute()
+        {
+             Dictionary<string, string> soapParams = new Dictionary<string, string>();
+            soapParams.Add("USCity", "Woodland Hills");
+            XmlDocument soapEnvelopeXml = CreateSoapEnvelope("http://www.webserviceX.NET", "GetInfoByCity", soapParams);
+
+            HttpWebRequest request = CreateWebRequest(@"http://www.webservicex.net/uszip.asmx");
+           
+            
+            using (Stream stream = request.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    string soapResult = rd.ReadToEnd();
+                    Console.WriteLine(soapResult);
+                }
+            }
+        }
+
+        private static XmlDocument CreateSoapEnvelope(string xmlns, string action, Dictionary<string,string> parameters)
+        {
+            var soapParameters = CreateParameters(parameters);
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            sb.AppendLine(@"
+                <Envelope xmlns=""http://schemas.xmlsoap.org/soap/envelope/"">
+                  <Body>");
+
+            sb.AppendLine($@"<{action} xmlns=""{xmlns}"">");
+            foreach (var item in parameters)
+            {
+                sb.Append($@"<{item.Key}>");
+                sb.Append($"{item.Value}");
+                sb.Append($@"</{item.Key}>");
+                sb.AppendLine("");
+
+            }
+            sb.AppendLine($@"</{action}>");
+            sb.AppendLine("</Body>");
+            sb.AppendLine($@"</Envelope>");
+            var soapEnv = sb.ToString();
+            soapEnvelopeXml.LoadXml(soapEnv);
+            return soapEnvelopeXml;
+        }
+
+        private static XmlDocument CreateParameters(Dictionary<string,string> soapParamters)
+        {
+            StringBuilder sb = new StringBuilder();
+    
+            foreach (var item in soapParamters)
+            {
+                sb.Append($@"<{item.Key}>");
+                sb.Append($"{item.Value}");
+                sb.Append($@"</{item.Key}>");
+                sb.AppendLine("");
+
+            }
+
+            XmlDocument soapBodyParameters = new XmlDocument();
+            soapBodyParameters.LoadXml(sb.ToString());
+            return soapBodyParameters;
+        }
+
+        /// <summary>
+        /// Create a soap webrequest to [Url]
+        /// </summary>
+        /// <returns></returns>
+        public static HttpWebRequest CreateWebRequest(string url)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add(@"SOAP:Action");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+    }
+}
